887c76b1c12787972a44541fb8d5cd9b484c5095
IWMTT <iwmtt0424@gmail.com>
2025-05-03 00:03:46 +0900

    modified: BellController.cs and unitypackage

--- scripts/BellController.cs
+++ scripts/BellController.cs
@@ -1,12 +1,9 @@
-﻿
-using System;
+﻿using System;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
-
-// ベルの動作をコントロールするためのコントローラスクリプト
-// このスクリプトを当てただけではベルは動作せず、外部からRing()関数を呼ぶ必要がある
+using UnityEditor;
 
 public class BellController : UdonSharpBehaviour
 {
@@ -15,7 +12,7 @@ public class BellController : UdonSharpBehaviour
     private DateTime _pressed_time;
     private DateTime _updated_time;
     private float _volume;
-    public bool isActivated = false; 
+    public bool isActivated = false;
     [SerializeField] private float animation_duration = 2.0f;
     void Start()
     {
@@ -27,18 +24,23 @@ public class BellController : UdonSharpBehaviour
 
     void Update()
     {
-        if (anim.GetBool("isRinging") == true) //is Ringing がtrueのとき＝ベルのアニメーションが呼ばれている状態
+        //is Ringing がtrueのとき＝ベルのアニメーションが呼ばれている状態
+        if (anim.GetBool("isRinging") == true)
         {
-            _updated_time = DateTime.Now; //経過時間の評価
-            if(IsTimePassed(_pressed_time, _updated_time, animation_duration)) //押された時間から、アニメーション終了時間が経っているか判定
+            //経過時間の評価
+            _updated_time = DateTime.Now;
+            //押された時間から、アニメーション終了時間が経っているか判定
+            if (IsTimePassed(_pressed_time, _updated_time, animation_duration))
             {
-                anim.SetBool("isRinging",false); //経っていたらfalseにする
+                //経っていたらfalseにする
+                anim.SetBool("isRinging", false);
                 isActivated = false;
             }
         }
         else //is Ringingがfalseのとき
         {
-            if (isActivated == true) //Ring()が呼ばれると、このif文内の処理が実行される
+
+            if (isActivated == true)
             {
                 anim.SetBool("isRinging", true);
                 _pressed_time = DateTime.Now;
@@ -47,17 +49,16 @@ public class BellController : UdonSharpBehaviour
             }
 
         }
+
     }
 
-    // ベルを鳴らす関数。別のスクリプトやオブジェクトからベルを鳴らせるように、publicに指定
     public void Ring(float input_volume)
     {
         isActivated = true;
         _volume = input_volume;
     }
 
-    // ある時間input_pressed_timeと、別の時間input_updated_timeを比較して、その差が基準時間thres_secを超えているかを判断する関数
-    private bool IsTimePassed(DateTime input_pressed_time, DateTime input_updated_time,float thres_sec)
+    private bool IsTimePassed(DateTime input_pressed_time, DateTime input_updated_time, float thres_sec)
     {
         TimeSpan _dt = input_updated_time - input_pressed_time;
         float _dtsec = (float)_dt.TotalMilliseconds / 1000;
@@ -70,5 +71,22 @@ public class BellController : UdonSharpBehaviour
         {
             return false;
         }
+    }
+}
+
+[CustomEditor(typeof(BellController))] 
+public class BellControllertEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Ring"))
+        {
+            BellController bellController = (BellController)target;
+            bellController.Ring(1.0f);
+        }
+
+
     }
 }
